97c8cb2ec403b3ce780eadd44ae4dfd5087b3590
Daniel Gorbe <daniel@gorbe.io>
2025-10-21 05:03:33 +0200

    Add Unmarshal() method to Response

--- response.go
+++ response.go
@@ -1,7 +1,11 @@
 package www
 
 import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
 	"io"
+	"mime"
 	"net/http"
 )
 
@@ -29,4 +33,25 @@ func ParseResponse(resp *http.Response) (*Response, error) {
 	r.Body, err = io.ReadAll(resp.Body)
 
 	return r, err
+}
+
+// Unmarshal unmarshals the [Response.Body] based on the response `Content-Type`:
+//   - [json.Unmarhsal] if `Content-Type` is `application/json`
+//   - [xml.Unmarshal]  if `Content-Type` is `application/xml`
+//   - [error] otherwise
+func (r *Response) Unmarshal(v any) error {
+
+	contentType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+	if err != nil {
+		return fmt.Errorf("failed to parse Content-Type: %w", err)
+	}
+
+	switch contentType {
+	case ContentTypeJSON:
+		return json.Unmarshal(r.Body, v)
+	case ContentTypeXML:
+		return xml.Unmarshal(r.Body, v)
+	default:
+		return fmt.Errorf("unknown Content-Type: %s", contentType)
+	}
 }
