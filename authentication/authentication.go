fb20e391b5171e281577ece8d1da0cf6e7cbd370
Daniel Gorbe <daniel@gorbe.io>
2025-11-08 22:40:39 +0100

    Update documentation

--- authentication/authentication.go
+++ authentication/authentication.go
@@ -1,5 +1,12 @@
-// Package authentication provides HTTP authentication
+/*
+Package authentication provides HTTP authentication
+
+The authentications are implemented with the [http.RoundTripper].
+The authentication header are added to every request with the RoundTrip() method.
+*/
 package authentication
+
 import "net/http"
 
+// Authentication is an alias type to [http.RoundTripper].
 type Authentication = http.RoundTripper
